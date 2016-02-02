4d453e01c776c5ab2a2ef4582f404af793e3a489
codegracie <grace.m.sojor@framgia.com>
2016-02-02 17:36:41 +0800

    Add AdminCategory Edit, Show, Update

--- db/migrate/20160202050618_create_activities.rb
+++ db/migrate/20160202050618_create_activities.rb
@@ -1,7 +1,7 @@
 class CreateActivities < ActiveRecord::Migration
   def change
     create_table :activities do |t|
-      t.integer :log_id
+      t.references :log, polymorphic: true
       t.string :content
       t.string :activity_type
 
